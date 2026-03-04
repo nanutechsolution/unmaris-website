049de7520211f4a522d524f5d76f4f3323c336ec
nanu <ranusate19@gmail.com>
2026-03-05 03:09:43 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,8 +7,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-
-        
         $this->call([
             UserSeeder::class,
             RolePermissionSeeder::class,

a191732c8f9e86f3eb741e5c04c7dc32bb6ddfd0
nanu <ranusate19@gmail.com>
2026-03-05 03:13:50 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->authGuard('web')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web([
             TrackVisitors::class, // Middleware untuk Melacak Pengunjung
-
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {

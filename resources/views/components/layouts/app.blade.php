698eee8e719cd34a54348cafa73cac933cf4d58d
nanu <ranusate19@gmail.com>
2026-03-06 00:56:20 +0800

    Update app.blade.php

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -366,7 +366,7 @@
             <!-- Body / Contacts -->
             <div class="p-5 space-y-3 bg-gray-50">
                 <!-- Kontak 1: CS PMB -->
-                <a href="https://wa.me/6281234567890?text=Halo%20Admin%20PMB%20UNMARIS,%20saya%20ingin%20bertanya%20seputar%20informasi%20pendaftaran%20mahasiswa%20baru." target="_blank" class="flex items-center gap-3 p-3 bg-white rounded-xl hover:shadow-lg transition-all border border-gray-100 group transform hover:-translate-y-0.5">
+                <a href="https://wa.me/62812-1615-6883?text=Halo%20Admin%20PMB%20UNMARIS,%20saya%20ingin%20bertanya%20seputar%20informasi%20pendaftaran%20mahasiswa%20baru." target="_blank" class="flex items-center gap-3 p-3 bg-white rounded-xl hover:shadow-lg transition-all border border-gray-100 group transform hover:-translate-y-0.5">
                     <div class="w-10 h-10 bg-blue-50 text-unmaris-blue rounded-full flex items-center justify-center shrink-0 group-hover:bg-[#25D366] group-hover:text-white transition-colors shadow-sm">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
@@ -379,7 +379,7 @@
                 </a>
 
                 <!-- Kontak 2: CS Akademik -->
-                <a href="https://wa.me/6281234567891?text=Halo%20Admin%20Akademik%20UNMARIS,%20saya%20ingin%20bertanya%20seputar%20perkuliahan%20dan%20fakultas." target="_blank" class="flex items-center gap-3 p-3 bg-white rounded-xl hover:shadow-lg transition-all border border-gray-100 group transform hover:-translate-y-0.5">
+                <a href="https://wa.me/62852-3980-3210?text=Halo%20Admin%20Akademik%20UNMARIS,%20saya%20ingin%20bertanya%20seputar%20perkuliahan%20dan%20fakultas." target="_blank" class="flex items-center gap-3 p-3 bg-white rounded-xl hover:shadow-lg transition-all border border-gray-100 group transform hover:-translate-y-0.5">
                     <div class="w-10 h-10 bg-yellow-50 text-yellow-600 rounded-full flex items-center justify-center shrink-0 group-hover:bg-[#25D366] group-hover:text-white transition-colors shadow-sm">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>

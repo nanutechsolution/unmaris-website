459b24014e82492774c7fe816027780e28cd258a
nanu <ranusate19@gmail.com>
2026-03-10 01:11:23 +0800

    Update FacilitySeeder.php

--- database/seeders/FacilitySeeder.php
+++ database/seeders/FacilitySeeder.php
@@ -14,31 +14,31 @@ class FacilitySeeder extends Seeder
             [
                 'name' => 'Laboratorium Komputer Terpadu',
                 'description' => 'Dilengkapi dengan PC spesifikasi tinggi dan koneksi internet cepat untuk menunjang praktikum mahasiswa IT dan Bisnis Digital.',
-                'image' => "logo-lab.png", // Placeholder gambar
+                'image' => "", // Placeholder gambar
                 'order' => 1,
             ],
             [
                 'name' => 'Perpustakaan Pusat',
                 'description' => 'Menyediakan ribuan koleksi buku cetak, jurnal internasional, dan area baca yang nyaman serta tenang untuk referensi mahasiswa.',
-                'image' => "logo-perpus.png", // Placeholder gambar
+                'image' => "", // Placeholder gambar
                 'order' => 2,
             ],
             [
                 'name' => 'Aula Serbaguna (Auditorium)',
                 'description' => 'Ruang pertemuan berkapasitas besar untuk menunjang kegiatan acara wisuda, seminar nasional, dan kegiatan kemahasiswaan skala besar.',
-                'image' => "logo-aula.png", // Placeholder gambar
+                'image' => "", // Placeholder gambar
                 'order' => 3,
             ],
             [
                 'name' => 'Kantin Mahasiswa',
                 'description' => 'Menyajikan berbagai pilihan makanan dan minuman dengan harga terjangkau untuk memenuhi kebutuhan gizi mahasiswa selama beraktivitas di kampus.',
-                'image' => "logo-kantin.png", // Placeholder gambar
+                'image' => "", // Placeholder gambar
                 'order' => 4,
             ],
             [
                 'name' => 'Lapangan Olahraga',
                 'description' => 'Area terbuka untuk berbagai cabang olahraga seperti basket dan voli guna mendukung kebugaran dan minat bakat mahasiswa UNMARIS.',
-                'image' => "logo-lapangan.png", // Placeholder gambar
+                'image' => "", // Placeholder gambar
                 'order' => 5,
             ],
         ];
